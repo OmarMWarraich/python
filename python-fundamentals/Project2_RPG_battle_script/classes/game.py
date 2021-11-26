8448676cd1ad6dcfa538d2def4127ac853d145ba
OmarMWarraich <earthling986@gmail.com>
2021-11-27 04:03:20 +0500

    Turning our magic into a class

--- python-fundamentals/Project2_RPG_battle_script/classes/game.py
+++ python-fundamentals/Project2_RPG_battle_script/classes/game.py
@@ -1,4 +1,6 @@
 import random
+from .magic import Spell
+import pprint
 
 class bcolors:
     HEADER = '\033[95m'
@@ -25,11 +27,6 @@ class Person:
     def generate_damage(self):
         return random.randrange(self.atkl, self.atkh)
 
-    def generate_spell_damage(self, i):
-        mgl = self.magic[i]["dmg"] - 5
-        mgh = self.magic[i]["dmg"] + 5
-        return random.randrange(mgl, mgh)
-
     def take_damage(self, dmg):
         self.hp -= dmg
         if self.hp < 0:
@@ -51,12 +48,6 @@ class Person:
     def reduce_mp(self, cost):
         self.mp -= cost
 
-    def get_spell_name(self, i):
-        return self.magic[i]["name"]
-
-    def get_spell_mp_cost(self, i):
-        return self.magic[i]["cost"]
-
     def choose_action(self):
         i = 1
         print(bcolors.OKBLUE + bcolors.BOLD + "Actions" + bcolors.ENDC)
@@ -66,9 +57,9 @@ class Person:
 
     def choose_magic(self):
         i = 1
+
         print(bcolors.OKBLUE + bcolors.BOLD + "Magic" + bcolors.ENDC)
         for spell in self.magic:
-            print(str(i) + ":", spell["name"])
-            # print(str(i) + ":", spell["name"], "(cost:", str(spell["mp"]) + ")")
+            print(str(i) + ":", spell.name, "(cost:", str(spell.cost) + ")")
             i += 1
 
