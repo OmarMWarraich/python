ee9bd7e48b91e56aec18b08da10454ca02339747
OmarMWarraich <earthling986@gmail.com>
2021-11-28 14:22:40 +0500

    Enemy HP bar

--- python-fundamentals/Project2_RPG_battle_script/main.py
+++ python-fundamentals/Project2_RPG_battle_script/main.py
@@ -1,7 +1,7 @@
 from classes.game import Person, bcolors
 from classes.magic import Spell
 from classes.inventory import Item
-
+import random
 
 # Create Black Magic
 fire = Spell("Fire", 10, 100, "black")
@@ -54,6 +54,8 @@ while running:
         player.get_stats()
 
     print("\n")
+
+    enemy.get_enemy_stats()
 
     for player in players:
 
@@ -109,17 +111,24 @@ while running:
                    player.heal(item.prop)
                    print(bcolors.OKGREEN + "\n" + item.name + " heals for", str(item.prop), "HP" + bcolors.ENDC)
               elif item.type == "elixir":
-                  player.hp = player.maxhp
-                  player.mp = player.maxmp
+                  if item.name == "MegaElixir":
+                      for i in players:
+                          i.hp = i.maxhp
+                          i.mp = i.maxmp
+                  else:
+
+                      player.hp = player.maxhp
+                      player.mp = player.maxmp
                   print(bcolors.OKGREEN + "\n" + item.name + " fully restores HP/MP" + bcolors.ENDC)
               elif item.type == "attack":
                   enemy.take_damage(item.prop)
                   print(bcolors.FAIL + "\n" + item.name + " deals", str(item.prop), "points of damage"  + bcolors.ENDC)
 
     enemy_choice = 1
-
+    target = random.randrange(0, 3)
     enemy_dmg = enemy.generate_damage()
-    player.take_damage(enemy_dmg)
+
+    players[target].take_damage(enemy_dmg)
     print("Enemy attacks for", enemy_dmg)
 
     print("=============================")
